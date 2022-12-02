cef6280461571d0ee734b072db87af8fe132d7ef
woeshiga <egor.grebenckin16@gmail.com>
2022-12-03 02:30:29 +0300

    update arithmetic.py file

---
+++ arithmetic.py
@@ -0,0 +1,8 @@
+print("4 * 100 - 54 = ?")
+
+answer = input("Send the answer: ")
+
+if answer == 4 * 100 - 54:
+    print("It's right!")
+else:
+    print(f"It's wrong! Right answer: {4 * 100 -54}")
